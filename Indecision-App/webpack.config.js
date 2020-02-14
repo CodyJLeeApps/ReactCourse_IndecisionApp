89e3d0197c073e44aca3e52a100b119be8086b0a
Cody <cody.j.lee@icloud.com>
2020-02-13 20:25:20 -0500

    Adding style-loader and css-loader

--- Indecision-App/webpack.config.js
+++ Indecision-App/webpack.config.js
@@ -1,4 +1,3 @@
-// entry -> output
 const path = require('path');
 
 module.exports = {
@@ -12,6 +11,12 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use:[
+                'style-loader',
+                'css-loader'
+            ]
         }]
     },
     devtool: 'cheap-module-eval-source-mapp',
